61910ae84eead6732701ac810666526c27f6f140
joaobrumlisboa <133824111+joaobrumlisboa@users.noreply.github.com>
2024-04-16 01:21:43 -0300

    Add files via upload

--- conexaobd/conexao.php
+++ conexaobd/conexao.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "1234";
+$password = "Batata123";
 $dbname = "pucdevacademy";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
