5b1f9dde8acfe60e3465c156961c3f1127f82d6c
Your Name <felipepoulos@gmail.com>
2024-04-15 21:27:49 -0300

    Todos os dados sendo encaminhados para o BD

--- cadastro/cadastro-bd.php
+++ cadastro/cadastro-bd.php
@@ -8,6 +8,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nomeFic = $conn->real_escape_string($_POST["nomeFic"]);
     $ramo = $conn->real_escape_string($_POST["ramo"]);
     $tel = $conn->real_escape_string($_POST["tel"]);
+    $nomeRepresentante = $conn->real_escape_string($_POST["nomeRepresentante"]);
+    $cpf = $conn->real_escape_string($_POST["cpf"]);
+    $cargo = $conn->real_escape_string($_POST["cargo"]);
+    $email = $conn->real_escape_string($_POST["email"]);
+    $senha = md5($_POST["senha"]);
 
     function verificarDadoExistente($conn, $campo, $valor) {
         $sql = "SELECT * FROM empresa WHERE $campo = '$valor'";
@@ -21,8 +26,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             VALUES ('$cnpj', '$nomeEmpresa', '$nomeFic', '$ramo', '$tel')";
 
             if ($conn->query($sqlEmpresa) === TRUE) {
-                header("Location: ../login/login.php");
-                exit(); 
+                // Cadastro do funcionário
+                $sqlFuncionario = "INSERT INTO funcionario (cpf, nome, cargo, email, senha, fk_Empresa_cnpj) 
+                                    VALUES ('$cpf', '$nomeRepresentante', '$cargo', '$email', '$senha', '$cnpj')";
+
+                if ($conn->query($sqlFuncionario) === TRUE) {
+                    header("Location: ../login/login.php");
+                    exit(); 
+                } else {
+                    echo "Erro ao cadastrar funcionário: " . $conn->error;
+                }
             } else {
                 echo "Erro ao cadastrar empresa: " . $conn->error;
             }
