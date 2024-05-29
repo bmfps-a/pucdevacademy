24f1ae79ec98746235cffa398eb7a018768bb079
joaobrumlisboa <133824111+joaobrumlisboa@users.noreply.github.com>
2024-05-28 21:53:32 -0300

    Erro de poder registrar sem confirmar senha arrumado

--- cadastro/validacao.js
+++ cadastro/validacao.js
@@ -17,6 +17,13 @@ function enviar() {
     let senha = document.getElementById("senha").value
     let confirmarSenha = document.getElementById("confirmarSenha").value
     let MensagemConfirmarSenha = document.getElementById("mensagem-confirmarSenha")
+    let campos = ["nome", "cpf", "ra", "tel", "email", "senha"];
+        
+    campos.forEach(function(campo) {
+        if (!validar(campo)) {
+            camposValidos = false;
+        }
+    });
     if (confirmarSenha=== senha){
         MensagemConfirmarSenha.textContent = ""
         camposValidos = true;
@@ -25,22 +32,11 @@ function enviar() {
         MensagemConfirmarSenha.textContent = "Senhas não iguais!"
         camposValidos = false
     }
-    let campos = ["nomeEmpresa", "cnpj", "nomeRepresentante", "nomeFic", "ramo", "cpf", "cargo", "tel", "email", "senha",];
-        
-    campos.forEach(function(campo) {
-        if (!validar(campo)) {
-            camposValidos = false;
-        }
-    });
     if (camposValidos === true) {
         console.log("Valores: ")
-        console.log(document.getElementById("nomeEmpresa").value)
-        console.log(document.getElementById("cnpj").value)
-        console.log(document.getElementById("nomeRepresentante").value)
-        console.log(document.getElementById("nomeFic").value)
-        console.log(document.getElementById("ramo").value)
+        console.log(document.getElementById("nome").value)
         console.log(document.getElementById("cpf").value)
-        console.log(document.getElementById("cargo").value)
+        console.log(document.getElementById("ra").value)
         console.log(document.getElementById("tel").value)
         console.log(document.getElementById("email").value)
         console.log(document.getElementById("senha").value)
@@ -50,25 +46,13 @@ function enviar() {
     return false
 }
 
+
 $(document).ready(function() {
-    $('#cnpj').inputmask('99.999.999/0009-99');
-    $('#nomeRepresentante').inputmask({
-        regex: "[A-Za-zÀ-ÖØ-öø-ÿçÇ ]*",
-        greedy: false
-    });
-    $('#nomeFic').inputmask({
-        regex: "[A-Za-zÀ-ÖØ-öø-ÿçÇ ]*",
-        greedy: false
-    });
-    $('#ramo').inputmask({
+    $('#nome').inputmask({
         regex: "[A-Za-zÀ-ÖØ-öø-ÿçÇ ]*",
         greedy: false
     });
     $('#cpf').inputmask('999.999.999-99');
-        
-    $('#cargo').inputmask({
-        regex: "[A-Za-zÀ-ÖØ-öø-ÿçÇ ]*",
-        greedy: false
-    });
+    $('#ra').inputmask('99999999');
     $('#tel').inputmask('(99) 99999-9999');   
 });
